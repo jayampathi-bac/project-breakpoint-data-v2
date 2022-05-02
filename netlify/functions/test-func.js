6f0e93cff5ab0f147a350d0095cf492395fc4d40
jayampathi-bac <jayampathi.bac@gmail.com>
2022-05-02 19:37:46 +0530

    testing using timeout

---
+++ netlify/functions/test-func.js
@@ -0,0 +1,22 @@
+exports.handler = async (event, context) => {
+    const name = event.queryStringParameters.name || "World";
+
+    let i = 60;
+    function onTimer() {
+        i--;
+        if (i < 0) {
+            console.log(' timeout over')
+        }
+        else {
+            console.log('timout ',i)
+            setTimeout(onTimer, 1000);
+        }
+    }
+
+    onTimer()
+
+    return {
+        statusCode: 200,
+        body: `Hello, ${name}`,
+    };
+};\
